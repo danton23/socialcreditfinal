7db347d36200c36a1ed72523bd778f3b25e798c0
Charlie <melvins_fan@hotmail.com>
2019-02-27 14:41:51 +0000

    second

--- scripts/main.js
+++ scripts/main.js
@@ -7,23 +7,3 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/douglas.jpg');
     }
-}
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-myButton.onclick = function() {
-  setUserName();
-}
